affc38ed4b349ec6af57ffba2435b1db54401eed
Haze Anderson <haze@hazeanderson.com>
2022-08-30 08:58:11 -0500

    correct test returns values

--- t/02-lat.py
+++ t/02-lat.py
@@ -8,8 +8,8 @@ class TestLoad(unittest.TestCase):
         mycopy = mylist.copy()
         self.assertEqual( lat(mylist), True, 'lat is a lat' )
         self.assertEqual( lat(['a','b','c']), True, 'lat is a lat' )
-        self.assertEqual( lat(['a',None,'c']), True, 'lat with empty element is not a lat' )
-        self.assertEqual( lat(['a',mylist,'c']), True, 'lat that contains another list is not a lat' )
+        self.assertEqual( lat(['a',None,'c']), False, 'lat with empty element is not a lat' )
+        self.assertEqual( lat(['a',mylist,'c']), False, 'lat that contains another list is not a lat' )
         self.assertEqual( lat('not a list'), False, 'atom is not a lat' )
         self.assertEqual( mylist, mycopy, 'list has not changed' )
 
