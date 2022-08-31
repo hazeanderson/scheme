cf0b0ed00485f2235dfdb872ae454c4c4e9518bf
Haze Anderson <haze@hazeanderson.com>
2022-08-31 12:32:25 -0500

    updated var name, small refactor, comments

--- schemer/common.py
+++ schemer/common.py
@@ -54,6 +54,7 @@ def rember(at, lat):
         return cons(car(lat), rember(at, cdr(lat)))
 
 # return new lat containing first elements from lat's lats
+# silly example
 def firsts(lolat):
     if null(lolat):
         return []
@@ -88,6 +89,7 @@ def subst(new, old, lat):
         return cons(car(lat), subst(new, old, cdr(lat)))
 
 # replace first occurance of o1 or 2 in lat with new
+# silly example
 def subst2(new, o1, o2, lat):
     if null(lat):
         return []
@@ -99,13 +101,13 @@ def subst2(new, o1, o2, lat):
         return cons(car(lat), subst2(new, o1, o2, cdr(lat)))
 
 # remove all occurances of a from lat
-def multirember(a, lat):
+def multirember(at, lat):
     if null(lat):
         return []
-    elif car(lat) == a:
-        return multirember(a, cdr(lat))
+    elif car(lat) == at:
+        return multirember(at, cdr(lat))
     else:
-        return cons(car(lat), multirember(a, cdr(lat)))
+        return cons(car(lat), multirember(at, cdr(lat)))
 
 # insert new atom into lat, to the left of all occurances of old
 def multiinsertL(new, old, lat):
@@ -121,7 +123,7 @@ def multiinsertR(new, old, lat):
     if null(lat):
         return []
     elif car(lat) == old:
-        return cons(car(lat), cons(new, multiinsertR(new, old, cdr(lat))))
+        return cons(old, cons(new, multiinsertR(new, old, cdr(lat))))
     else:
         return cons(car(lat), multiinsertR(new, old, cdr(lat)))
 
@@ -133,7 +135,4 @@ def multisubst(new, old, lat):
         return cons(new, multisubst(new, old, cdr(lat)))
     else:
         return cons(car(lat), multisubst(new, old ,cdr(lat)))
-
-
-
 
